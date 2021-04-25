bb1a2cd859dbaa757e65b6027588be6038bdc90b
najmus2808 <najmus2808@gmail.com>
2021-04-25 18:58:26 +1000

    done !

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import User from './components/User/User';
 
 const App = () => {
+  const [users, setUSers] = useState([]);
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then(response => response.json())
+      .then(data => setUSers(data));
+  }, [])
   return (
     <div>
-      App
-      <User></User>
+      {
+        users.map(user => <User user={user} key={user.id}></User>)
+      }
     </div>
   );
 };
--- src/components/User/User.js
+++ src/components/User/User.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import './User.css'
 
-const User = () => {
+const User = (props) => {
+    const user = props.user;
     return (
-        <div>
-            User
+        <div className="user-container">
+            <h2>Name: {user.name}</h2>
+            <h3>Roll: {user.id}</h3>
+            <h4>Email: {user.email}</h4>
+            <h4>Phone: {user.phone}</h4>
+
         </div>
     );
 };
